33d83f082d740877b3c84a3d78464281f480aaf7
Mark Larah <mark@Marks-MacBook-Pro-3.local>
2013-10-20 12:56:19 +0100

    index.php classified

--- index.php
+++ index.php
@@ -1,44 +1,25 @@
 <?php
-ini_set("memory_limit","64M");
-include "FaceDetector.php";
+ini_set("memory_limit","128M");
+set_time_limit(0);
 include "image.php";
 
-$source   = new Image('lena512color.jpg');
+$source   = new Image('mark.jpg');
 $overlay  = new Image('goathead.png');
 
-$imageSource = array(
-	'name' => 'lena512color.jpg',
-	'type' => 'jpeg'
-);
-
-$imageSource['size'] = getimagesize($imageSource['name']);
-
-$imageOverlay = array(
-	'name' => 'goathead.png',
-	'type' => 'png'
-);
-$imageOverlay['size'] = getimagesize($imageOverlay['name']);
-$detector = new svay\FaceDetector('detection.dat');
-
-$detector->faceDetect($imageSource['name']);
-$face = $detector->getFace();
-$faceOffset = array(
-	'x' => -50,
-	'y' => -100
-);
-$facePos = array(
-	'x' => $face['x'] + $faceOffset['x'],
-	'y' => $face['y'] + $faceOffset['y'],
-);
+$source->getFace();
 
 header('Content-Type: image/jpeg');
-$canvas = imagecreatetruecolor(512, 512);
+$canvas = imagecreatetruecolor($source->size[0], $source->size[1]);
 
-$imageSource['func'] = 'imagecreatefrom' . $imageSource['type'];
-$imageOverlay['func'] = 'imagecreatefrom' . $imageOverlay['type'];
-$imageSource['img'] = $imageSource['func']($imageSource['name']);
-$imageOverlay['img'] = $imageOverlay['func']($imageOverlay['name']);
+$faceOffset = array(
+  'x' => -50,
+  'y' => -100
+);
+$facePos = array(
+  'x' => $source->face['x'] + $faceOffset['x'],
+  'y' => $source->face['y'] + $faceOffset['y'],
+);
 
-imagecopy($canvas, $imageSource['img'], 0, 0, 0, 0, $imageSource['size'][0], $imageSource['size'][1]);
-imagecopy($canvas, $imageOverlay['img'], $facePos['x'], $facePos['y'], 0, 0, $imageOverlay['size'][0], $imageOverlay['size'][1]);
+imagecopy($canvas, $source->img, 0, 0, 0, 0, $source->size[0], $source->size[1]);
+imagecopy($canvas, $overlay->img, $facePos['x'], $facePos['y'], 0, 0, $overlay->size[0], $overlay->size[1]);
 imagejpeg($canvas);
